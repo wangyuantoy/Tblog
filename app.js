a330b6ad31de036b01713d3e704a68dc86c02d53
wangyuan <wangyuan@maim9.com>
2017-03-13 14:03:50 +0800

    add new files

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'html');
 app.engine('html',require('ejs').renderFile);
 
 
-app.use(favicon(path.join(__dirname, 'public','images', 'favicon.ico')));
+//app.use(favicon(path.join(__dirname, 'public','images', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
+app.use('/user', users);
 app.use('/article', articles);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
